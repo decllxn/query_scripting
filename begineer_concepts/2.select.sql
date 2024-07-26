c9752194c4bf8bce24493e7fdd04d4a4e6ad0639
Declan Munene <munene.declan1@gmail.com>
2024-07-26 19:10:32 +0300

    The select statement

---
+++ begineer_concepts/2.select.sql
@@ -0,0 +1,11 @@
+ -- The select statement
+ -- The SELECT statement is used to select data from a database
+ 
+ -- Example
+SELECT CustomerName, City FROM Customers
+
+-- Syntax
+SELECT column1, column2,...
+FROM table_name;
+-- column1 and column2 are the field names of the table you want to select data FROM
+-- the table_name represents the name of the table you want to select data FROM
