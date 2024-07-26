951a634994a1d8c5b9aaa3ea3e5eac905ac231eb
Declan Munene <munene.declan1@gmail.com>
2024-07-26 19:03:53 +0300

    SQL learning

--- begineer_concepts/1.intro.sql
+++ begineer_concepts/1.intro.sql
@@ -1,1 +1,25 @@
-
+-- sql stands for structured query language
+ -- It lets you manipulate databases
+ -- sql creates, retrieves, updated and delete
+ -- There are different versions of the sql language
+ -- RDBMS stands for relational Database Management System
+ -- The data in RDBMS is stored in database objects called tables
+ 
+ 
+ SELECT * FROM CUSTOMERS
+ -- The above is an example of a query
+ -- The above query statement returns all records from a table
+ -- named "Customers":
+ 
+ -- Examples of common SQL commands
+ SELECT - -- Extracts data from a database
+ UPDATE - -- updates data in a database
+ DELETE - -- deletes data from a database
+ INSERT INTO - --inserts new data into a database
+ CREATE DATABASE - -- creates a new database
+ ALTER DATABASE - -- modifies a database
+ CREATE TABLE - -- creates a new table
+ ALTER TABLE - -- modifies a table
+ DROP TABLE - -- deletes a table
+ CREATE INDEX - -- creates an index(search key)
+ DROP INDEX - -- deletes an indes
