0a2359f6af8dcfb8bb4a2cd11dbd8464f11228bd
Declan Munene <munene.declan1@gmail.com>
2024-09-05 09:47:59 +0300

    Sql starting

---
+++ sql/1.intro.sql
@@ -0,0 +1,14 @@
+-- SQL is a standard lamguage for storing, manipulating and retrieving data in databases
+-- SQL is a standard language for accessing and manipulating databases
+-- SQL - Structured Query language
+-- RDBMS stabnds for relational database management systems
+SELECT * FROM Customers
+
+-- Example of SQL
+-- Every table is broken up into smaller entities called fields
+
+
+-- SQL SYNTAX
+-- Most of the action you need to perform on a database are done with sql statements
+-- SQL statements consists of keywords that are easy to understand
+-- A database most often contains one or more tables, each table is identified by a name
